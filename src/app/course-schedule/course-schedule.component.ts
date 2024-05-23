8d0ddc51e2e805ba20b329cfe2c0ac63514495e8
Alexander Hilding <hildingx@gmail.com>
2024-05-23 10:25:05 +0200

    First commit. Initialized angular project.

---
+++ src/app/course-schedule/course-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-schedule.component.html',
+  styleUrl: './course-schedule.component.css'
+})
+export class CourseScheduleComponent {
+
+}
