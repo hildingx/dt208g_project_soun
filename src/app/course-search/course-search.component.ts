2821d57c9c007919f501eb35e0debaf3d72e6028
Alexander Hilding <hildingx@gmail.com>
2024-05-23 14:38:35 +0200

    Lagt till sök/filtrering-funktionalitet.

--- src/app/course-search/course-search.component.ts
+++ src/app/course-search/course-search.component.ts
@@ -3,17 +3,20 @@ import { Course } from '../models/course';
 import { CourseDataService } from '../services/course-data.service';
 import { CommonModule } from '@angular/common';
 import { ScheduleService } from '../services/schedule.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-course-search',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './course-search.component.html',
   styleUrl: './course-search.component.css'
 })
 export class CourseSearchComponent {
   //Egenskaper
   courseList: Course[] = [];
+  filteredCourseList: Course[] = [];
+  filterValue: string = "";
 
   constructor(private courseData: CourseDataService, private scheduleService: ScheduleService) {}
 
@@ -21,7 +24,15 @@ export class CourseSearchComponent {
   ngOnInit() {
     this.courseData.getCourses().subscribe(data => {
       this.courseList = data;
+      this.filteredCourseList = data;
     });
+  }
+
+  applyFilter(): void {
+    this.filteredCourseList = this.courseList.filter((course) =>
+      course.courseCode.toLowerCase().includes(this.filterValue.toLowerCase()) ||
+      course.courseName.toLowerCase().includes(this.filterValue.toLowerCase())
+    );
   }
 
   addToSchedule(course: Course) {
