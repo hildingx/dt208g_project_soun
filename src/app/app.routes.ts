7920df8d0e60fb08d6e1b87348b02844533713e1
Alexander Hilding <hildingx@gmail.com>
2024-05-23 11:39:20 +0200

    Satt upp routing och komponenter. Gjort en CourseSearchComponent för visning av kurser och (så småningom) filtrering. Implementerat CourseDataService för hämtning av kurser från JSON-fil. Lagt till routes för course-search och course-schedule. Skriver ut lista på kurser under course-search.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CourseSearchComponent } from './course-search/course-search.component';
+import { CourseScheduleComponent } from './course-schedule/course-schedule.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "courses", component: CourseSearchComponent },
+    { path: "schedule", component: CourseScheduleComponent},
+    { path: "", redirectTo: "course-search", pathMatch: "full" }
+];
--- src/app/course-search/course-search.component.ts
+++ src/app/course-search/course-search.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Course } from '../models/course';
+import { CourseDataService } from '../services/course-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-course-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course-search.component.html',
   styleUrl: './course-search.component.css'
 })
 export class CourseSearchComponent {
+  //Egenskaper
+  courseList: Course[] = [];
 
+  constructor(private courseData: CourseDataService) {}
+
+  //Metoder
+  ngOnInit() {
+    this.courseData.getCourses().subscribe(data => {
+      this.courseList = data;
+    });
+  }
 }
